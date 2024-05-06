821b40839e9a3d33791dede3426856a5043deb24
phiarchitect <phi@phiarchitect.com>
2024-05-05 18:37:35 -0700

    update website

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -1,12 +1,13 @@
-from ablog.conf import *
+#  from ablog.conf import *
+from photon_platform.sphinxilator.global_conf import *
 import photon_platform.curator as module
+
 version = module.__version__
 
-org = 'photon-platform'
-org_name = 'PHOTON platform'
+org = "photon-platform"
+org_name = "photon-platform"
 
-repo = 'curator'
-repo_name = 'curator'
+repo = "curator"
+repo_name = "curator"
 
 setup_globals(org, org_name, repo, repo_name)
-
